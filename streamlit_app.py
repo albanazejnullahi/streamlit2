4448671e67c6827e0fedf86fd5ab0e3c31ab4f17
albanazejnullahi <albana.zejnullahi@radix.com>
2024-07-30 13:16:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,9 @@ def main():
         # Further filter to include only rows where Assessment is not empty
         filtered_df = filtered_df[filtered_df['Assessment'].str.strip().astype(bool)]
 
+        # Limit to top 10 properties
+        filtered_df = filtered_df.head(10)
+
         st.header("Filtered Data")
 
         if filtered_df.empty:
